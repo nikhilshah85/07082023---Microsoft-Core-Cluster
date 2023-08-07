5161a69173359c6af7ab68b21ef8b32612ba9fe4
Nikhil <nikhilshah.net@hotmail.com>
2023-08-07 11:26:24 +0530

    Day 1 - MVC intro

---
+++ Day 1/firstMVC/firstMVC/Controllers/productController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using firstMVC.Models;
+namespace firstMVC.Controllers
+{
+    public class productController : Controller
+    {
+
+        productList pObj = new productList(); //this is not good, we should use DI - dependency injection instead of this code
+
+        public IActionResult ProductsList()
+        {
+            //view bag is a variable which will take data from controller to view
+            //viewbad is getting data from model
+            ViewBag.products = pObj.GetAllProducts();
+            return View();
+        }
+
+    }
+}
---
+++ Day 1/firstMVC/firstMVC/Models/productList.cs
@@ -0,0 +1,34 @@
+﻿namespace firstMVC.Models
+{
+    public class productList
+    {
+        #region Properties
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public string productCategory { get; set; }
+        public double productPrice { get; set; }
+        public bool productIsInStock { get; set; }
+        #endregion
+
+        #region Data
+        static List<productList> pList = new List<productList>()
+        {
+            new productList(){ productId=101, productName="Iphone", productCategory="Electronics", productIsInStock=true, productPrice=150000},
+            new productList(){ productId=102, productName="Pepsi", productCategory="Cold-Drink", productIsInStock=true, productPrice=80},
+            new productList(){ productId=103, productName="Latte", productCategory="Coffee", productIsInStock=false, productPrice=250},
+            new productList(){ productId=104, productName="Maggie", productCategory="Fast-Food", productIsInStock=true, productPrice=160},
+            new productList(){ productId=105, productName="Air-Pods", productCategory="Electronics", productIsInStock=true, productPrice=26000},
+            new productList(){ productId=106, productName="Nike Air", productCategory="Sports", productIsInStock=false, productPrice=18000},
+            new productList(){ productId=107, productName="Adidas T", productCategory="Sports", productIsInStock=true, productPrice=3450},
+        };
+        #endregion
+
+        #region Methods
+        public List<productList> GetAllProducts()
+        {
+            return pList;
+        }
+        #endregion
+
+    }
+}
