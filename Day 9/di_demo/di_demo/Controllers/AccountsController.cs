43fb8dd0de32f96bfc172fd52a65ec22dc7e6834
Nikhil <nikhilshah.net@hotmail.com>
2023-08-18 11:16:24 +0530

    Day 9 - DI

---
+++ Day 9/di_demo/di_demo/Controllers/AccountsController.cs
@@ -0,0 +1,26 @@
+﻿using di_demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace di_demo.Controllers
+{
+    public class AccountsController : Controller
+    {
+
+        //  accounts accObj = new accounts(); //this not good, use DI insted
+
+        accounts _accObj; //this object will be created by runtime and will be injected inside controller
+
+        //tell runtime to inject the object here
+        public AccountsController(accounts _accObjREF)
+        {
+            _accObj = _accObjREF;
+        }
+
+
+        public IActionResult DisplayAccounts()
+        {
+            ViewBag.acc = _accObj.GetAllAccounts();
+            return View();
+        }
+    }
+}
---
+++ Day 9/di_demo/di_demo/Models/accounts.cs
@@ -0,0 +1,27 @@
+﻿namespace di_demo.Models
+{
+    public class accounts
+    {
+        public int accNo{ get; set; }
+        public string accName{ get; set; }
+        public string accType{ get; set; }
+        public double accBalance{ get; set; }
+        public bool accIsActive{ get; set; }
+
+
+        static List<accounts> accList = new List<accounts>()
+        {
+            new accounts(){ accNo=101, accName="Nikhil", accBalance=9000, accIsActive=true, accType="Savings"},
+            new accounts(){ accNo=102, accName="Nikita", accBalance=9000, accIsActive=true, accType="Savings"},
+            new accounts(){ accNo=103, accName="Nirav", accBalance=9000, accIsActive=false, accType="Savings"},
+            new accounts(){ accNo=104, accName="Rohan", accBalance=9000, accIsActive=true, accType="Current"},
+            new accounts(){ accNo=105, accName="Mohan", accBalance=9000, accIsActive=true, accType="Savings"},
+            new accounts(){ accNo=106, accName="Ramesh", accBalance=9000, accIsActive=true, accType="Current"}
+        };
+
+        public List<accounts> GetAllAccounts()
+        {
+            return accList;
+        }
+    }
+}
