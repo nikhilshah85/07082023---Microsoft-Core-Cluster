b7d485e2238a3a2032a98ec5572c74f2990699ea
Nikhil <nikhilshah.net@hotmail.com>
2023-08-21 11:25:54 +0530

    Day 10 - CORS

---
+++ Day 10/employeeAPI/employeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace employeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/employee/list")]
+        public IActionResult EmployeeNames()
+        {
+            List<string> empNames = new List<string>()
+            {
+                "Nikhil","Karan","Mohan","Karthik","Akshay","Rohan", "Sumit"
+
+            };
+
+            return Ok(empNames);
+        }
+    }
+}
